97479c36385911333a786d8db341b6a1b51c6faf
githubiscoolforeverything <61217991+githubiscoolforeverything@users.noreply.github.com>
2020-03-11 21:08:45 -0400

    Update test.php

--- otherpages/test.php
+++ otherpages/test.php
@@ -1,1 +1,13 @@
-
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+  <form action="send.php" method="post" name="test">
+    <p>
+      Email:
+      <input name="email" type="text" />
+    </p>
+    <p><textarea name="message" cols="5" rows="5"></textarea>&nbsp;</p>
+    <input type="submit" id="submit" value="Submit" />
+    </body>
+  </html>
